b3c3b70d0271188bf16ec634b2c0d24dcf59893a
joaoflaviosantos <joaoflavio.s@hotmail.com>
2023-12-25 23:14:17 -0300

    refactor: Embracing best practices for global variable declarations in the file

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,9 +1,20 @@
+# External Dependencies
 from fastapi.testclient import TestClient
 
-from src.main import app
+# Local Dependencies
 from src.core.config import settings
-from .helper import _get_token
+from tests.helper import _get_token
+from src.main import app
 
+# Test data: 'common user'
+TEST_NAME = settings.TEST_NAME
+TEST_USERNAME = settings.TEST_USERNAME
+TEST_EMAIL = settings.TEST_EMAIL
+TEST_PASSWORD = settings.TEST_PASSWORD
+
+# Test data: admin/superuser 'test' credentials
+ADMIN_USERNAME = settings.ADMIN_USERNAME
+ADMIN_PASSWORD = settings.ADMIN_PASSWORD
 
 client = TestClient(app)
 
@@ -11,17 +22,17 @@ def test_post_user(client: TestClient) -> None:
     response = client.post(
         "/api/v1/system/user",
         json = {
-            "name": settings.TEST_NAME,
-            "username": settings.TEST_USERNAME,
-            "email": settings.TEST_EMAIL,
-            "password": settings.TEST_PASSWORD
+            "name": TEST_NAME,
+            "username": TEST_USERNAME,
+            "email": TEST_EMAIL,
+            "password": TEST_PASSWORD
         }
     )
     assert response.status_code == 201
 
 def test_get_user(client: TestClient) -> None:
     response = client.get(
-        f"/api/v1/system/user/{settings.TEST_USERNAME}"
+        f"/api/v1/system/user/{TEST_USERNAME}"
     )
     assert response.status_code == 200
 
@@ -33,15 +44,15 @@ def test_get_multiple_users(client: TestClient) -> None:
 
 def test_update_user(client: TestClient) -> None:
     token = _get_token(
-        username=settings.TEST_USERNAME, 
-        password=settings.TEST_PASSWORD, 
+        username=TEST_USERNAME, 
+        password=TEST_PASSWORD, 
         client=client
     )
     
     response = client.patch(
-        f"/api/v1/system/user/{settings.TEST_USERNAME}",
+        f"/api/v1/system/user/{TEST_USERNAME}",
         json={
-            "name": f"Updated {settings.TEST_NAME}"
+            "name": f"Updated {TEST_NAME}"
         },
         headers={"Authorization": f'Bearer {token.json()["access_token"]}'}
     )
@@ -49,26 +60,26 @@ def test_update_user(client: TestClient) -> None:
 
 def test_delete_user(client: TestClient) -> None:
     token = _get_token(
-        username=settings.TEST_USERNAME, 
-        password=settings.TEST_PASSWORD, 
+        username=TEST_USERNAME, 
+        password=TEST_PASSWORD, 
         client=client
     )
 
     response = client.delete(
-        f"/api/v1/system/user/{settings.TEST_USERNAME}",
+        f"/api/v1/system/user/{TEST_USERNAME}",
         headers={"Authorization": f'Bearer {token.json()["access_token"]}'}
     )
     assert response.status_code == 200
 
 def test_delete_db_user(client: TestClient) -> None:
     token = _get_token(
-        username=settings.ADMIN_USERNAME, 
-        password=settings.ADMIN_PASSWORD, 
+        username=ADMIN_USERNAME, 
+        password=ADMIN_PASSWORD, 
         client=client
     )
 
     response = client.delete(
-        f"/api/v1/system/db_user/{settings.TEST_USERNAME}",
+        f"/api/v1/system/db_user/{TEST_USERNAME}",
         headers={"Authorization": f'Bearer {token.json()["access_token"]}'}
     )
     assert response.status_code == 200
