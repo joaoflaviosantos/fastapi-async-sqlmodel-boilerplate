45babed358f91ff3147934b8da7711908ca715dd
joaoflaviosantos <joaoflavio.s@hotmail.com>
2023-12-27 23:04:40 -0300

    test: change in the test organization format (1 file per API version + 1 file for common core)
    This change aims to organize tests in a more modular way, maintaining a separate file for each API version and consolidating common functionalities in a file dedicated to the core. This improves readability, maintenance, and scalability of the tests, making it easier to identify specific test cases for each API version and common functions shared by the core. This modularized approach promotes a clearer structure and facilitates future expansions and maintenance in the test code.

---
+++ src/apps/system/users/tests/v1.py
@@ -0,0 +1,83 @@
+# Third-Party Dependencies
+from fastapi.testclient import TestClient
+
+# Local Dependencies
+from src.core.config import settings
+from tests.helper import _get_token
+
+# Test data: 'common user'
+TEST_NAME = settings.TEST_NAME
+TEST_USERNAME = settings.TEST_USERNAME
+TEST_EMAIL = settings.TEST_EMAIL
+TEST_PASSWORD = settings.TEST_PASSWORD
+
+# Test data: admin/superuser 'test' credentials
+ADMIN_USERNAME = settings.ADMIN_USERNAME
+ADMIN_PASSWORD = settings.ADMIN_PASSWORD
+
+
+def test_post_user(client: TestClient) -> None:
+    response = client.post(
+        "/api/v1/system/user",
+        json = {
+            "name": TEST_NAME,
+            "username": TEST_USERNAME,
+            "email": TEST_EMAIL,
+            "password": TEST_PASSWORD
+        }
+    )
+    assert response.status_code == 201
+
+def test_get_user(client: TestClient) -> None:
+    response = client.get(
+        f"/api/v1/system/user/{TEST_USERNAME}"
+    )
+    assert response.status_code == 200
+
+def test_get_multiple_users(client: TestClient) -> None:
+    response = client.get(
+        "/api/v1/system/users"
+    )
+    assert response.status_code == 200
+
+def test_update_user(client: TestClient) -> None:
+    token = _get_token(
+        username=TEST_USERNAME, 
+        password=TEST_PASSWORD, 
+        client=client
+    )
+    
+    response = client.patch(
+        f"/api/v1/system/user/{TEST_USERNAME}",
+        json={
+            "name": f"Updated {TEST_NAME}"
+        },
+        headers={"Authorization": f'Bearer {token.json()["access_token"]}'}
+    )
+    assert response.status_code == 200
+
+def test_delete_user(client: TestClient) -> None:
+    token = _get_token(
+        username=TEST_USERNAME, 
+        password=TEST_PASSWORD, 
+        client=client
+    )
+
+    response = client.delete(
+        f"/api/v1/system/user/{TEST_USERNAME}",
+        headers={"Authorization": f'Bearer {token.json()["access_token"]}'}
+    )
+    assert response.status_code == 200
+
+def test_delete_db_user(client: TestClient) -> None:
+    token = _get_token(
+        username=ADMIN_USERNAME, 
+        password=ADMIN_PASSWORD, 
+        client=client
+    )
+
+    response = client.delete(
+        f"/api/v1/system/db_user/{TEST_USERNAME}",
+        headers={"Authorization": f'Bearer {token.json()["access_token"]}'}
+    )
+    assert response.status_code == 200
---
+++ tests/test_main.py
@@ -0,0 +1,8 @@
+# Third-Party Dependencies
+from fastapi.testclient import TestClient
+
+# Local Dependencies
+# --------------------------------------
+# --------- SYSTEM.USERS TESTS ---------
+# --------------------------------------
+from src.apps.system.users.tests.v1 import *
