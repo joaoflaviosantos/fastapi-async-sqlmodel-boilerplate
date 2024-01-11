743ae6cfea4aa6e68d6c137da8f573924a2a1ff6
joaoflaviosantos <joaoflavio.s@hotmail.com>
2024-01-11 00:06:27 -0300

    chore: improve logging for tests
    Enhance the logging setup for unit tests in 'tests/test_main.py':
    
    - Introduce a dedicated log file 'tests.log' to consolidate logs from unit tests.
    - Configure logging for the 'tests' module to provide clear and organized test-related logs.
    
    This change aims to streamline the logging process for unit testing, making it easier to track and analyze logs specific to the test suite.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,13 @@
 # Local Dependencies
+from src.core.logger import logging, configure_logging
+
+# Configure logging for the worker
+configure_logging(log_file='tests')
+
+# Logger instance for the current module
+logger = logging.getLogger(__name__)
+
+# Unit tests imports
 from src.apps.system.auth.tests.test_v1 import *
 from src.apps.system.users.tests.test_v1 import *
 from src.apps.system.tiers.tests.test_v1 import *
