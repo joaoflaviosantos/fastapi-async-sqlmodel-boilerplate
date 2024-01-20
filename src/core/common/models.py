ae8a928899b7765421cf93607ba64e5f67338f76
joaoflaviosantos <joaoflavio.s@hotmail.com>
2024-01-20 03:18:05 -0300

    chore: Update TimestampMixin with options for 'updated_at'
    Enhance the TimestampMixin in the API to provide two options for handling 'updated_at'. Option 1 keeps 'updated_at' always up-to-date, suitable for most scenarios. Option 2, recommended for frequent soft deletions and restorations, preserves the original timestamp of the last real modification. The commit includes detailed documentation on pros and cons for each option, helping developers make informed choices.

--- src/core/common/models.py
+++ src/core/common/models.py
@@ -4,7 +4,7 @@ from uuid import UUID, uuid4
 from typing import Optional
 
 # Third-Party Dependencies
-from sqlmodel import SQLModel, Column, Field, DateTime, func
+from sqlmodel import SQLModel, Field, DateTime
 
 
 # Define a base class for declarative models with support for dataclasses
@@ -29,6 +29,46 @@ class UUIDMixin(SQLModel):
 class TimestampMixin(SQLModel):
     """
     Adds 'created_at' and 'updated_at' fields with default values for the creation timestamp and update timestamp.
+
+    Note: By default, 'updated_at' is set to the current timestamp on every update, which is useful for tracking the last
+    modification time. However, in scenarios where soft deletion is performed and records may be restored, you might want
+    to consider the following options:
+
+    ----------------------------------------------------------
+
+    Option 1 (Recommended for most scenarios):
+    - Pros:
+        - Keeps 'updated_at' always up-to-date, providing an accurate timestamp for the last modification.
+        - Suitable for most use cases where soft deletion is not a common scenario.
+
+    - Cons:
+        - May lead to inaccurate information if soft deletion and restoration are part of the system's workflow.
+
+    updated_at: datetime = Field(
+        sa_type=DateTime(timezone=True),
+        default_factory=lambda: datetime.now(UTC),
+        sa_column_kwargs={"onupdate": datetime.now(UTC)},
+        description="Timestamp for the last update of the record",
+    )
+
+    ----------------------------------------------------------
+
+    Option 2 (Recommended for scenarios with frequent soft deletions and restorations):
+    - Pros:
+        - Preserves the original timestamp of the last real modification even after a soft delete and restore.
+        - Avoids potential inaccuracies caused by automatic updates to 'updated_at' during soft deletions.
+
+    - Cons:
+        - 'updated_at' will not be automatically updated on every change, potentially affecting accuracy if the field
+        needs to reflect every modification.
+        - Currently, the BaseCRUD's update method in the API automatically handles the update of 'updated_at'. If other
+        update methods are used that do not go through this mechanism, 'updated_at' may not be updated as expected.
+
+    updated_at: Optional[datetime] = Field(
+        sa_type=DateTime(timezone=True),
+        default_factory=lambda: datetime.now(UTC),
+        description="Timestamp for the last update of the record",
+    )
     """
 
     # Data Columns
