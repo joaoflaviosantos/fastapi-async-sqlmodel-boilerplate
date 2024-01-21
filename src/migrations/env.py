8493c526aab91dc067a064ef8d335d97e5fe9a4b
joaoflaviosantos <joaoflavio.s@hotmail.com>
2024-01-21 11:18:32 -0300

    chore: migrate from SQLAlchemy to SQLModel (Alembic configs)
    In this commit, we make changes to 'env.py' and 'script.py.mako' files in the Alembic directory to ensure a smooth transition from using SQLAlchemy to SQLModel in our application.
    
    Changes in 'env.py':
    - Updated SQLAlchemy configurations to use the Postgres async URI from application settings.
    - Adjusted comments and docstrings for better clarity and understanding.
    - Modified naming conventions for SQLModel/SQLAlchemy to align with project standards.
    - Implemented a filtering mechanism for database objects to exclude specific index types.
    
    Changes in 'script.py.mako':
    - Reviewed and updated migration script templates to reflect the changes in the naming conventions.
    - Ensured compatibility with SQLModel by making necessary adjustments.
    
    These changes are part of an ongoing effort to enhance the overall maintainability and performance of our application by adopting SQLModel as our primary database toolkit.

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -13,10 +13,10 @@ from src.core.common.models import Base
 from src.core.config import settings
 from src.core.db import *
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+# The Alembic Config object providing access to the .ini file values.
 config = context.config
 
+# Set the SQLAlchemy URL using the Postgres async URI from settings.
 config.set_main_option(name="sqlalchemy.url", value=f"{settings.POSTGRES_ASYNC_URI}")
 
 # Interpret the config file for Python logging.
@@ -24,53 +24,61 @@ config.set_main_option(name="sqlalchemy.url", value=f"{settings.POSTGRES_ASYNC_U
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
+# Add your model's MetaData object here (for 'autogenerate' support)
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
+# Setting naming conventions for SQLModel/SQLAlchemy
+target_metadata.naming_convention = {
+    "ix": "ix_%(column_0_label)s",  # Index
+    "uq": "uq_%(table_name)s_%(column_0_name)s",  # Unique constraint
+    "ck": "ck_%(table_name)s_%(constraint_name)s",  # Check constraint
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",  # Foreign key
+    "pk": "pk_%(table_name)s",  # Primary key
+}
+
+# Other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
 
-def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    This function is responsible for running migrations when the application is not connected to a database.
-
+def filter_db_objects(
+    object,  # noqa: indirect usage
+    name,
+    type_,
+    *args,  # noqa: indirect usage
+    **kwargs,  # noqa: indirect usage
+):
     """
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
+    Filter the database objects based on the given criteria.
 
-    with context.begin_transaction():
-        context.run_migrations()
+    Args:
+        object: The database object to be filtered. # noqa: indirect usage
+        name: The name of the database object.
+        type_: The type of the database object.
+        *args: Additional positional arguments. # noqa: indirect usage
+        **kwargs: Additional keyword arguments. # noqa: indirect usage
+
+    Returns:
+        bool: True if the object should be included, False if it should be filtered out.
+    """
+    if type_ == "index" and name.startswith("idx") and name.endswith("geom"):
+        return False
+
+    return True
 
 
 def do_run_migrations(connection: Connection) -> None:
     """Perform the actual migration process.
 
     This function is responsible for running migrations when the application is connected to a database.
-
     """
     context.configure(connection=connection, target_metadata=target_metadata)
 
     with context.begin_transaction():
+        context.configure(
+            connection=connection, target_metadata=target_metadata, include_object=filter_db_objects
+        )
         context.run_migrations()
 
 
@@ -78,13 +86,12 @@ async def run_async_migrations() -> None:
     """Run migrations asynchronously.
 
     This function is responsible for running migrations in an asynchronous manner.
-
     """
-
     connectable = async_engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
+        future=True,
     )
 
     async with connectable.connect() as connection:
@@ -93,13 +100,32 @@ async def run_async_migrations() -> None:
     await connectable.dispose()
 
 
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL and not an Engine, though an Engine is acceptable here as well.
+    By skipping the Engine creation, we don't even need a DBAPI to be available.
+
+    This function is responsible for running migrations when the application is not connected to a database.
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+        include_object=filter_db_objects,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
 
     This function is responsible for running migrations when the application is online.
-
     """
-
     asyncio.run(run_async_migrations())
 
 
