9d597c85a43aebf4cfbd13caffb4da06e5bd930b
joaoflaviosantos <joaoflavio.s@hotmail.com>
2025-01-02 01:27:59 -0300

    refactor: remove automatic commit in async_get_db, improve error handling with logging, and update docstring

--- backend/src/core/db/session.py
+++ backend/src/core/db/session.py
@@ -1,11 +1,16 @@
 # Third-Party Dependencies
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
 from typing import AsyncGenerator
 
 # Local Dependencies
 from src.core.config import settings
+from src.core.logger import logging
+
+# Logger instance for the current module
+logger = logging.getLogger(__name__)
 
 # Define the database URI and URL based on the application settings
 DATABASE_URI = f"{settings.POSTGRES_ASYNC_URI}"
@@ -20,8 +25,34 @@ local_session = sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_c
 
 # Define an async function to get the database session
 async def async_get_db() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Provides an async database session for use within application logic.
+
+    This function yields a database session from the session factory and ensures
+    that any errors during execution trigger a rollback to maintain database consistency.
+
+    Note
+    ----
+    - Commits must be performed explicitly in the application code where necessary.
+
+    Yields
+    ------
+    AsyncSession
+        The database session to be used for database operations.
+
+    Raises
+    ------
+    SQLAlchemyError
+        If a database error occurs, the transaction is rolled back,
+        and the error is logged before being re-raised.
+    """
+
     async_session = local_session
 
     async with async_session() as db:
-        yield db
-        await db.commit()
+        try:
+            yield db
+        except SQLAlchemyError as e:
+            await db.rollback()
+            logger.error(f"Database error: {e}")
+            raise
